4fd9c973bd8ee7a143aa1504d1535944896c7479
Maurice Beckford <mreese914@gmail.com>
2021-05-12 11:25:21 -0400

    adding files

--- static/js/app.js
+++ static/js/app.js
@@ -25,33 +25,33 @@ function buildTable(data) {
 
 // 1. Create a variable to keep track of all the filters as an object.
 
-var Filter = {};
+var filters = {};
 // 3. Use this function to update the filters. 
 function updateFilters() {
 
   // 4a. Save the element that was changed as a variable.
-  let changedElement = d3.select("this");
+  let changedElement = d3.select(this);
   // 4b. Save the value that was changed as a variable.
   let changedValue = changedElement.property("value");
   
   // 4c. Save the id of the filter that was changed as a variable.
-  let changedId = changedValue.attr("id");
+  let changedId = changedElement.attr("id");
   
 
   // 5. If a filter value was entered then add that filterId and value
   // to the filters list. Otherwise, clear that filter from the filters object.
   if (changedValue) {
-    filter[filterId] = changedValue;
+    filters[changedId] = changedValue;
 
   }
   else {
-    delete filter[filterId];
+    delete filters[changedId];
 
   }
 
 
   // 6. Call function to apply all filters and rebuild the table
-  filterTable(filters);
+  filterTable();
 
 }
 
@@ -63,7 +63,9 @@ function filterTable() {
 
   // 9. Loop through all of the filters and keep any data that
   // matches the filter values
-  Object.entries(filters).forEach(([key, value]) => { filteredData = filteredData.filter(row => row[key] === value); });
+  Object.entries(filters).forEach(([key, value]) => {
+     filteredData = filteredData.filter(row => row[key] === value);
+     });
 
   // 10. Finally, rebuild the table using the filtered data
 
